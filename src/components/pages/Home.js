c74bdde57b3e32de3c5eb4bbb634db10cdc20484
shaizai1 <117948803+shaizai1@users.noreply.github.com>
2023-03-08 17:32:40 +0000

    added styling and updated homepage

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -3,16 +3,14 @@ import React from "react";
 function Home() {
   return (
     <div>
-      <h1>Home Page</h1>
+      <h1>Shaiza Iqbal</h1>
+      <img src="https://avatars.githubusercontent.com/u/117948803?v=4">
+      </img>
+      <ul>
+        <li>Professional Front-End Developer</li>
+      </ul>
       <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque velit, lobortis ut magna
-        varius, blandit rhoncus sem. Morbi lacinia nisi ac dui fermentum, sed luctus urna tincidunt.
-        Etiam ut feugiat ex. Cras non risus mi. Curabitur mattis rutrum ipsum, ut aliquet urna
-        imperdiet ac. Sed nec nulla aliquam, bibendum odio eget, vestibulum tortor. Cras rutrum
-        ligula in tincidunt commodo. Morbi sit amet mollis orci, in tristique ex. Donec nec ornare
-        elit. Donec blandit est sed risus feugiat porttitor. Vestibulum molestie hendrerit massa non
-        consequat. Vestibulum vitae lorem tortor. In elementum ultricies tempus. Interdum et
-        malesuada fames ac ante ipsum primis in faucibus.
+        Welcome to my portfolio of work! I hope to balance logic and creativity, in order to create beautiful and functional applications. 
       </p>
     </div>
   );

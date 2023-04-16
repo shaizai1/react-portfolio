ec0fcb5eeb7d16771ed12feb3804a10843eefb8b
shaizai1 <117948803+shaizai1@users.noreply.github.com>
2023-04-16 23:24:33 +0100

    updated homepage

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -3,14 +3,10 @@ import React from "react";
 function Home() {
   return (
     <div>
+      <br></br>
+      <br></br>
       <img src="https://github.com/shaizai1/react-portfolio/blob/main/assets/images/logo1.png?raw=true">
       </img>
-      <ul>
-        <li>Web Developer</li>
-      </ul>
-      <p>
-        Welcome to my portfolio of work! I hope to balance logic and creativity, in order to create beautiful and functional applications. 
-      </p>
     </div>
   );
 }
